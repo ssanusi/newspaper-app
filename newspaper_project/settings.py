a1b96b04d5d5c03e1a11c5596f1534dd8179a126
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-11-20 20:04:30 +0100

    added bootstrap and crispy_forms

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd Party
+    'crispy_forms',
+
+    #Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -125,3 +129,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
